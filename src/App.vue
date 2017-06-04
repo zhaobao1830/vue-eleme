a6e225848a7444bc9ee97bdc99fdb0f1610d4702
zhaobao1830 <x5219438>
2017-06-05 00:52:38 +0800

    完成了主体框架和一像素样式

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,15 @@
   <div id="app">
     <v-header></v-header>
     <div class="tab">
-      <div class="tab-item">商品</div>
-      <div class="tab-item">评论</div>
-      <div class="tab-item">商家</div>
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
     </div>
     <div class="content">
       i am content
@@ -29,6 +35,9 @@
     .tab
       display: flex
       width: 100%
+      height: 40px
+      line-height: 40px
       .tab-item
         flex: 1
+        text-align: center
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,26 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-
+import Goods from '../components/goods/goods';
+import Ratings from '../components/ratings/ratings';
+import Seller from '../components/seller/seller';
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'index'
+      path: '/goods',
+      name: 'goods',
+      component: Goods
+    },
+    {
+      path: '/ratings',
+      name: 'ratings',
+      component: Ratings
+    },
+    {
+      path: '/seller',
+      name: 'seller',
+      component: Seller
     }
   ]
 });
