fb7706665217686c052cbda98ec03f1ad9bc9da2
zhaobao1830 <x5219438>
2017-06-04 22:50:11 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+    <div class="content">
+      i am content
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import header from '@/components/header/header.vue';
+
+  export default {
+    name: 'app',
+    components: {
+      'v-header': header
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" type="text/stylus">
+  #app
+    .tab
+      display: flex
+      width: 100%
+      .tab-item
+        flex: 1
+</style>
