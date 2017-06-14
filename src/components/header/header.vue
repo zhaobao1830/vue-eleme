75b23f94f2efc33a744af287fdc912aca1e742a7
zhaobao1830 <x5219438>
2017-06-15 00:13:39 +0800

    完成header组件  star

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -30,7 +30,12 @@
       <img :src="seller.avatar" width="100%" height="100%">
     </div>
     <div v-show="detailShow" class="detail">
-
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+        </div>
+      </div>
+      <div class="detail-close"></div>
     </div>
   </div>
 </template>
@@ -184,4 +189,22 @@ export default {
       backdrop-filter: blur(10px)
       opacity: 1
       background-color: rgba(7, 17, 27, 0.8)
+      .detail-wrapper
+        width: 100%
+        min-height: 100%
+        .detail-main
+          margin-top: 64px
+          padding-bottom: 64px
+          .name
+            line-height: 16px
+            text-align: center
+            font-size: 16px
+            font-weight: 700
+      .detail-close
+        position: relative
+        width: 32px
+        height: 32px
+        margin: -64px auto 0 auto
+        clear: both
+        font-size: 32px
 </style>
